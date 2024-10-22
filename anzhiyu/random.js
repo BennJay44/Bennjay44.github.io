134fe80962e560378a5a74e2be401b651e106b6a
Bennjay <2707167603@qq.com>
2024-10-22 16:55:01 +0800

    Site updated: 2024-10-22 16:55:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/49bb27f6.html","posts/c8a95ac9.html"];function toRandomPost(){
+var posts=["posts/49bb27f6.html","posts/c8a95ac9.html","posts/710db6ea.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

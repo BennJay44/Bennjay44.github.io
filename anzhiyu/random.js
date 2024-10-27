7703bc8982861b77bc51fe462052b4e1e28ac028
Bennjay <2707167603@qq.com>
2024-10-27 11:50:47 +0800

    Site updated: 2024-10-27 11:50:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/49bb27f6.html","posts/c8a95ac9.html","posts/710db6ea.html","posts/192a7d07.html"];function toRandomPost(){
+var posts=["posts/49bb27f6.html","posts/c8a95ac9.html","posts/710db6ea.html","posts/192a7d07.html","posts/a63eeb01.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

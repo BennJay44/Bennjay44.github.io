edc7f03347e4104c8cc3d898e052b550aafc5d03
Bennjay <2707167603@qq.com>
2024-11-11 16:58:06 +0800

    Site updated: 2024-11-11 16:58:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/49bb27f6.html","posts/c8a95ac9.html","posts/710db6ea.html","posts/192a7d07.html","posts/a63eeb01.html"];function toRandomPost(){
+var posts=["posts/49bb27f6.html","posts/c8a95ac9.html","posts/710db6ea.html","posts/192a7d07.html","posts/a63eeb01.html","posts/bf0489f3.html","posts/31c0f3ab.html","posts/94978a2f.html","posts/8b924896.html","posts/75f97bc3.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
